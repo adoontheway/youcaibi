e73fb5dc5f52dc13f07a63fa142edd48d8a67824
Ado <adoontheway@gmail.com>
2022-04-26 10:45:26 +0800

    add upload feature

--- stream/defs.go
+++ stream/defs.go
@@ -2,4 +2,6 @@ package main
 
 const (
 	VIDEO_DIR = "./videos/"
+	// max upload size 50M
+	MAX_UPLOAD_SIZE = 1024 * 1024 * 50
 )
--- stream/handlers.go
+++ stream/handlers.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -12,11 +13,12 @@ import (
 // 一个handler就是一个goroutine?
 func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	vid := p.ByName("vid")
-	vl := VIDEO_DIR + vid
+	vl := VIDEO_DIR + vid + ".mp4"
 
 	video, err := os.Open(vl)
 	if err != nil {
-		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		log.Printf("Error when open file :%s", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error") //500
 		return
 	}
 	w.Header().Set("Content-Type", "video/mp4")
@@ -25,7 +27,33 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	uname := p.ByName("username")
-	// vid := p.ByName("vid")
-	io.WriteString(w, uname)
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		sendErrorResponse(w, http.StatusBadRequest, "File is too big") //400
+		return
+	}
+	// _ FileHeader 用于文件格式验证等操作
+	file, _, err := r.FormFile("file") //<form name="file"> key is file
+	if err != nil {
+		log.Printf("Read file stream error:%s", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Read file error:%s", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	fileName := p.ByName("vid")
+	err = ioutil.WriteFile(VIDEO_DIR+fileName, data, 0666) // no 0777 权限太大
+	if err != nil {
+		log.Printf("Write file error:%s", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	sendNormalResponse(w, "Updateloaded succesfully", http.StatusCreated)
 }
